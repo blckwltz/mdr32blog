447706de25e0b2b3bed4572c4bba1aef43e7a789
Ruslan Breev <32988951+blckwltz@users.noreply.github.com>
2024-01-28 15:12:03 +0200

    fix typo

--- src/lib/constants/common.ts
+++ src/lib/constants/common.ts
@@ -1,4 +1,4 @@
-export const APP_HOST = 'https://blckwltz.githud.io';
+export const APP_HOST = 'https://blckwltz.github.io';
 export const BASE_PATH =
   typeof process.env.BASE_PATH !== 'undefined' && !!(process.env.BASE_PATH + '').trim() ? process.env.BASE_PATH : '';
 export const IMAGES_PATH = `${BASE_PATH}/images`;
